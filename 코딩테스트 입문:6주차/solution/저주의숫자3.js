6da010c339a0e3e62a0bc2ecf07afcf6577c43b4
apple <apple@apples-MacBook-Air.local>
2025-09-09 19:02:14 +0900

    저주의숫자3 풀이 추가

--- 코딩테스트 입문:6주차/solution/저주의숫자3.js
+++ 코딩테스트 입문:6주차/solution/저주의숫자3.js
@@ -6,3 +6,19 @@ function solution(n) {
 
   while (result % 3 === 0) result++;
 }
+
+// 스터디 이후 고친 코드
+function solution(n) {
+    let arr = [];
+    for(let i = 1; i <= n*3; i++){ // 3의 배수가 없는 3배크기의 배열 생성
+        if(i%3 !== 0)
+            arr.push(i);
+    }
+    
+    const result = arr.filter(el => { // 그 중 3을 포함하지 않는 숫자만 filter
+        const num = el+"";
+        return !num.includes('3');
+    })
+    
+    return result[n-1]; // 해당 인덱스가 그 숫자 
+}
